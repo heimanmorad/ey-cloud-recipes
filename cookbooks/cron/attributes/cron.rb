97790c2c40b6c86b229b65119a374274ab1f1667
morad heiman <heimanmorad@gmail.com>
2016-01-09 21:46:29 +0200

    fixed wrong cron

--- cookbooks/cron/attributes/cron.rb
+++ cookbooks/cron/attributes/cron.rb
@@ -6,4 +6,4 @@ default[:custom_crons] = [{:name => "backup-s3-60-minutes", :time => "0 */1 * * 
 {:name => "Store.more_zap_cron at 4am", :time => "10 18 * * *", :command => "cd /data/konimboil/current && ./script/runner -e production 'Store.more_zap_cron'", :instance_name => "cron"},
 {:name => "Store.backup_stores at 7:20 am", :time => "20 21 * * *", :command => "cd /data/konimboil/current && ./script/runner -e production 'Store.backup_stores'", :instance_name => "cron"},
 {:name => "backup-s3-1-day", :time => "30 20 * * *", :command => "cd /data/konimboil/current && RAILS_ENV=production bundle exec rake backup:run trigger='production-backup-s3-every-day-ey'", :instance_name => "cron"},
-{:name => "Buy2.buy2_cron", :time => "30 5 * * *", :command => "cd /data/konimboil/current && RAILS_ENV=production bundle exec rake backup:run trigger='production-backup-s3-every-day-ey'", :instance_name => "cron"}]
+{:name => "Buy2.buy2_cron", :time => "30 4 * * *", :command => "cd /data/konimboil/current && ./script/runner -e production 'Buy2.buy2_cron'", :instance_name => "cron"}]
